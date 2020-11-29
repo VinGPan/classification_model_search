99f47ca618fbb600bc4bd9ee2511098ae1e1b3f8
vinay <vinay@savormetrics.com>
2020-11-29 11:44:45 -0500

    - fixes to logging

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -1,12 +1,21 @@
 # https://stackoverflow.com/questions/13733552/logger-configuration-to-log-to-file-and-print-to-stdout
 
 import logging
+import os
+from datetime import datetime
+
+try:
+    os.makedirs("output/log/")
+except:
+    pass
+
+date_time = datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("output/exp.log"),
+        logging.FileHandler("output/log/" + date_time + ".log"),
         logging.StreamHandler()
     ]
 )
